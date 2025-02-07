ab2bf6b92ddd1074d4a8d313186e16f0e0efa58e
mayurwagh <mayurwagh1412@gmail.com>
2025-02-07 16:27:27 +0530

    backend configuration is done

---
+++ pipeline.groovy
@@ -0,0 +1,25 @@
+pipeline {
+    agent any 
+    stages {
+        stage ('pull'){
+            steps {
+                git branch: 'dev', url: 'https://github.com/mayurmwagh/CDEC-B21-Backend.git'
+            }
+        }
+        stage ('build'){
+            steps {
+                sh 'mvn clean package'
+            }
+        }
+        stage ('deploy'){
+            steps {
+                sh '''
+                    docker build . -t mayurwagh/CDEC-B21-Backend:latest 
+                    docker push mayurwagh/CDEC-B21-Backend:latest
+                    docker rmi mayurwagh/CDEC-B21-Backend:latest
+                    kubectl apply -f ./yaml/
+                '''
+            }
+        }
+    }
+}
